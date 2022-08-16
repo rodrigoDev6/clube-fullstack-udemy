49595d92b6ad2243bb25eae04471e8a20017bda8
rodrigodev6 <rodrigolima.alves21@gmail.com>
2022-08-16 09:44:14 -0300

    form get, post e filtros com php

--- curso-php/phpParaQuemNaoEntende/variaveisDeAmbientePhp/index.php
+++ curso-php/phpParaQuemNaoEntende/variaveisDeAmbientePhp/index.php
@@ -1,8 +1,9 @@
 <?php
 
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-var_dump($_ENV);
+// var_dump($_ENV);
+var_dump($_SERVER);
