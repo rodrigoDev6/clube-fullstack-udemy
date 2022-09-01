79e269f879498832e543e4f68f47e0719f6c43bf
rodrigodev6 <rodrigolima.alves21@gmail.com>
2022-09-01 20:39:31 -0300

    up

--- curso-php/phpParaQuemEntende/php-devclass/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-// session_regenerate_id(); generate new idSession
-
-require 'vendor/autoload.php';
--- curso-php/phpParaQuemNaoEntende/tiposDados.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// string, integer, float, bool, null
-
-//usando uma variavel na string:
-$name = 'rodrigo';
-echo "Olá {$name}"; // aspas simples não reotnam varivaeis dentro delas, só aspas duplas
