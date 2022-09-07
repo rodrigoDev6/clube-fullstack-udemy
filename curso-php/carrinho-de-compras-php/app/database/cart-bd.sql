c92b86bf4d84eaf0c06566715ab8bb8863fdfd26
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-09-07 09:24:34 -0300

    banco de dados

---
+++ curso-php/carrinho-de-compras-php/app/database/cart-bd.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `cart_udemy`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` DECIMAL(9,2) NOT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO 'products' ('name', 'price') VALUES ('Cabo HDMi',       '2314');
+INSERT INTO 'products' ('name', 'price') VALUES ('Tv Smart',        '5587');
+INSERT INTO 'products' ('name', 'price') VALUES ('Motorola',        '4872');
+INSERT INTO 'products' ('name', 'price') VALUES ('Iphone',          '8125');
+INSERT INTO 'products' ('name', 'price') VALUES ('Moto 0km',        '3000');
+INSERT INTO 'products' ('name', 'price') VALUES ('Mercedez benz',   '8854');
+INSERT INTO 'products' ('name', 'price') VALUES ('Fifa 21',         '7651');
+INSERT INTO 'products' ('name', 'price') VALUES ('Monitor AOC',     '8472');
